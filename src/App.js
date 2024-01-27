c5c0b9d75259cab86ed8cee48f975300a8571be8
jugglingdev <kaylamarie1785@gmail.com>
2024-01-26 20:16:46 -0600

    Refactor board squares

--- src/App.js
+++ src/App.js
@@ -9,6 +9,29 @@ function Square({ value, onSquareClick }) {
   );
 }
 
+function renderBoardRows(squares, onSquareClick) {
+  const board = [];
+
+  for (let row = 0; row < 3; row++) {
+    const squaresInRow = [];
+
+    for (let col = 0; col < 3; col++) {
+      const index = row * 3 + col;
+      squaresInRow.push(
+        <Square key={index} value={squares[index]} onSquareClick={() => onSquareClick(index)} />
+      );
+    }
+
+    board.push(
+      <div key={row} className='board-row'>
+        {squaresInRow}
+      </div>
+    );
+  }
+
+  return board;
+}
+
 function Board({ xIsNext, squares, onPlay }) {
 
   function handleClick(i) {
@@ -29,30 +52,17 @@ function Board({ xIsNext, squares, onPlay }) {
 
   const winner = calculateWinner(squares);
   let status;
+
   if (winner) {
-    status = winner + ' wins!';
+    status = `${winner} wins!`;
   } else {
-    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    status = `It's ${(xIsNext ? 'X' : 'O')}'s turn`;
   }
 
   return (
     <>
       <div className='status'>{status}</div>
-      <div className='board-row'>
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className='board-row'>
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className='board-row'>
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {renderBoardRows(squares, handleClick)}
     </>
   );
 }
